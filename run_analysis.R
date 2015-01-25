61a82468b8a61b4de610445eb09c0545e18648d7
Kevin Bolick <kbolick@hotmail.com>
2015-01-25 08:53:23 -0500

    Final Commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
-#KB
+# Prepares a tidy data that can be used for later analysis
+# Data for the project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# Performs the following:
 #1. Merges the training and the test sets to create one data set.
 #2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 #3. Uses descriptive activity names to name the activities in the data set
